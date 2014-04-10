f93dfae630a647b5a1db8dd73a2bde58fa1a7905
everix1992 <on3sh0t@ksu.edu>
2014-04-10 13:50:52 -0500

    Affix Work

--- Diablo3GearHelper/Types/Affix.cs
+++ Diablo3GearHelper/Types/Affix.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +16,13 @@ namespace Diablo3GearHelper.Types
     public enum AffixType
     {
         // Primary Stats
+        [EnumMember(Value = "Intelligence_Item")]
         Intelligence,
+        [EnumMember(Value = "Dexterity_Item")]
         Dexterity,
+        [EnumMember(Value = "Strength_Item")]
         Strength,
+        [EnumMember(Value = "Vitality_Item")]
         Vitality,
 
         // Offensive Stats
@@ -31,37 +36,80 @@ namespace Diablo3GearHelper.Types
 
         // Defensive Stats
         PercentLife,
+        [EnumMember(Value = "Armor_Bonus_Item")]
         BonusArmor,
         Thorns,
         AllResistances,
         FireResistance,
         ColdResistance,
+        [EnumMember(Value = "")]
+        LightningResistance,
+        [EnumMember(Value = "")]
+        ArcaneResistance,
+        [EnumMember(Value = "")]
+        ReducedDamageFromRangedAttacks,
 
         // Elemental Damage
+        [EnumMember(Value = "")]
         ArcaneDamage,
+        [EnumMember(Value = "")]
         FireDamage,
+        [EnumMember(Value = "")]
         ColdDamage,
+        [EnumMember(Value = "")]
         LightningDamage,
 
         // Healing
+        [EnumMember(Value = "")]
         LifeOnHit,
+        [EnumMember(Value = "")]
         LifePerKill,
+        [EnumMember(Value = "")]
         LifePerSecond,
+        [EnumMember(Value = "")]
+        HealthGloveAndPotionExtraLife,
 
         // Utility
+        [EnumMember(Value = "")]
         ResourceCostReduction,
+        [EnumMember(Value = "")]
         CooldownReduction,
+        [EnumMember(Value = "")]
         GoldPickupRadius,
+        [EnumMember(Value = "")]
+        BonusExperienceOnKill,
+        [EnumMember(Value = "")]
+        BonusExperiencePercent,
+        [EnumMember(Value = "")]
+        MovementSpeed,
+        [EnumMember(Value = "")]
+        ExtraGoldFromMonsters,
 
         // Sockets
-        Sockets1,
-        Sockets2,
-        Sockets3,
+        [EnumMember(Value = "Sockets")]
+        Sockets,
 
-        // Skills Damage
+        // Wizard Skills
+        [EnumMember(Value = "")]
+        ArcaneTorrentDamage,
+        [EnumMember(Value = "")]
+        ArcaneOrbDamage,
+        [EnumMember(Value = "")]
+        DistintegrateDamage,
+        [EnumMember(Value = "")]
+        BlackHoleDamage,
+
+        // Wizard Utilities
+        [EnumMember(Value = "")]
+        ArcanePowerOnCrit,
+        [EnumMember(Value = "")]
+        MaximumArcanePower,
 
         // Miscellaneous
+        [EnumMember(Value = "")]
         ReducedLevelRequirement,
+        [EnumMember(Value = "")]
+        ChanceToImmobilizeOnHit,
     }
 
     public class Affix
